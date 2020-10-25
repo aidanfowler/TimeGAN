471d69a78ca8a80269ad7a920bf44562ba179a76
Aidan <fowler.aidan@gmail.com>
2020-10-25 16:20:54 -0400

    fix resizing

--- sketch.js
+++ sketch.js
@@ -15,7 +15,6 @@ function setup() {
   if(url.indexOf('frame') != -1){
     showOneFrame = true;
   }
-  //fullscreen(true); //uncomment this if you are viewing in p5 editor otherwise image will be in weird spot
   bg = loadImage('gradientMeshSmall.jpg'); //load background image
   createCanvas(windowWidth, windowHeight); //make canvas size of window
   model = new rw.HostedModel({ 
@@ -56,7 +55,7 @@ function draw() {
   else if (showOneFrame){
     if(!oneFrameLoaded){
       background(bg);
-      image(new_image, displayWidth/2-imageWidth/2, (displayHeight/2-imageHeight/2), imageWidth, imageHeight);
+      image(new_image, windowWidth/2-imageWidth/2, (windowHeight/2-imageHeight/2), imageWidth, imageHeight);
       oneFrameLoaded = true;
     }
   }
@@ -67,11 +66,11 @@ function draw() {
       //image(bg,0,0,displayWidth, displayHeight);
       if (old_image) {
         tint(255, 255 - (frameCount % 255));
-        image(old_image, displayWidth/2-imageWidth/2, (displayHeight/2-imageHeight/2), imageWidth, imageHeight);
+        image(old_image, windowWidth/2-imageWidth/2, (windowHeight/2-imageHeight/2), imageWidth, imageHeight);
       }
       if (current_image) {
         tint(255, frameCount % 255);
-        image(current_image, displayWidth/2-imageWidth/2, (displayHeight/2-imageHeight/2), imageWidth, imageHeight);
+        image(current_image, windowWidth/2-imageWidth/2, (windowHeight/2-imageHeight/2), imageWidth, imageHeight);
       }
     }
     if (frameCount % 100 == 0) {
